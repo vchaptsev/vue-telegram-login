3913b9ff8a3f5451deefc7cde56ac68ae62b0129
Victor Chaptsev <chaptsev.ifmo@gmail.com>
2018-02-25 02:27:34 +0300

    feat: example added

---
+++ src/example.vue
@@ -0,0 +1,32 @@
+<template>
+  
+  <!-- Callback mode -->
+  <vue-telegram-login 
+    mode="callback"
+    telegram-login="YourTelegramBot"
+    @callback="yourCallbackFunction" />
+  
+  <!-- Redirect mode -->
+  <vue-telegram-login 
+    mode="redirect"
+    telegram-login="YourTelegramBot"
+    redirect-url="https://your-website.io" />
+
+</template>
+
+<script>
+import {vueTelegramLogin} from 'vue-telegram-login'
+
+export default {
+  name: 'your-component',
+  components: {vueTelegramLogin},
+  methods: {
+    yourCallbackFunction (user) {
+      // gets user as an input
+      // id, first_name, last_name, username,
+      // photo_url, auth_date and hash
+      console.log(user)
+    }
+  }
+}
+</script>
